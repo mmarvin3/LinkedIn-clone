df2dd07811f521ec0fa8953b9bc45437c190e0c0
mmarvin3 <mmerida336@gmail.com>
2023-05-19 18:52:59 -0400

    Header component setup

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React from 'react';
 import './App.css';
 import Header from "./Header"
 
+
 function App() {
   return (
     <div className="a pp">
-      <h1>Let's build a LinkedIn Clone</h1>
       {/* Header */}
       <Header />
 
--- src/Header.js
+++ src/Header.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import "./Header.css"
 import SearchIcon from '@mui/icons-material/Search';
- 
- function Header() {
-   return (
-    <div className='header'>
-        <h1>This is the header</h1>
+import LinLogo from "./Assests/Linkedin-Logo.png"
+import HeaderOption from './HeaderOption';
 
-        <div className='header_left'>
-            <img src='' alt=''/>
+function Header() {
+    return (
+        <div className='header'>
 
-            <div className='header__search'>
-                <SearchIcon />
-                <input type="text"/>
+            <div className='header_left'>
+                <img src={LinLogo} alt='' />
+
+                <div className='header_search'>
+                    <SearchIcon />
+                    <input type="text" />
+                </div>
+            </div>
+
+            <div className='header_right'>
+                <HeaderOption title="Home" />
+                <HeaderOption />
             </div>
         </div>
-        <div className='header_right'>
+    )
+}
 
-        </div>
-    </div>
-   )
- }
- 
- export default Header
+export default Header
