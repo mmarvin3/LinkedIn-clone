4966940665ecc1a2bcd8f40dc57d8a3954599b2f
mmarvin3 <mmerida336@gmail.com>
2023-05-20 16:12:47 -0400

    Header bar with 6 icons added

--- src/Header.js
+++ src/Header.js
@@ -3,6 +3,12 @@ import "./Header.css"
 import SearchIcon from '@mui/icons-material/Search';
 import LinLogo from "./Assests/Linkedin-Logo.png"
 import HeaderOption from './HeaderOption';
+import HomeIcon from '@mui/icons-material/Home';
+import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import ChatIcon from '@mui/icons-material/Chat';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import Me from "./Assests/Me.jpg"
 
 function Header() {
     return (
@@ -18,8 +24,12 @@ function Header() {
             </div>
 
             <div className='header_right'>
-                <HeaderOption title="Home" />
-                <HeaderOption />
+                <HeaderOption Icon={HomeIcon} title="Home" />
+                <HeaderOption Icon={SupervisorAccountIcon} title="My Network"/>
+                <HeaderOption Icon={BusinessCenterIcon} title="Jobs"/>
+                <HeaderOption Icon={ChatIcon} title="Messaging"/>
+                <HeaderOption Icon={NotificationsIcon} title="Notifications"/>
+                <HeaderOption avatar={Me} title="me"></HeaderOption>
             </div>
         </div>
     )
