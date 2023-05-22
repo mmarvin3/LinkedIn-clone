2802cb2746766acc6abe54ad9ff07f2ba98d43ca
mmarvin3 <mmerida336@gmail.com>
2023-05-22 15:30:39 -0400

    Post Component WIP

--- src/Feed.js
+++ src/Feed.js
@@ -2,6 +2,10 @@ import React from 'react'
 import CreateIcon from '@mui/icons-material/Create';
 import './Feed.css'
 import InputOption from './InputOption';
+import ImageIcon from '@mui/icons-material/Image';
+import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
+import EventNoteIcon from '@mui/icons-material/EventNote';
+import CalendarViewDayIcon from '@mui/icons-material/CalendarViewDay';
 
 function Feed() {
   return (
@@ -14,10 +18,15 @@ function Feed() {
                     <button type='submit'>Send</button>
                 </form>
             </div>
-            <div className='"feed__inputOptions'>
-                <InputOption />
+            <div className='feed__inputOptions'>
+                <InputOption Icon={ImageIcon} title='Photo' color="#70B5F9"/>
+                <InputOption Icon={SubscriptionsIcon} title='Video' color="#E7A33E"/>
+                <InputOption Icon={EventNoteIcon} title='Event' color="#C0CBCD"/>
+                <InputOption Icon={CalendarViewDayIcon} title='Write article' color="#7FC15E"/>
             </div>
         </div>
+
+        {/* Posts */}
     </div>
   )
 }
---
+++ src/Post.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Post.css"
+
+function Post({name, description, message, photoURL}) {
+  return (
+    <div className='post'>
+        
+    </div>
+  )
+}
+
+export default Post
