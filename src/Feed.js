f90c6ee4bd071b576c3083072c72dc0b37972bcb
mmarvin3 <mmerida336@gmail.com>
2023-05-22 16:22:20 -0400

    Post Section WIP 90%

--- src/Feed.js
+++ src/Feed.js
@@ -6,29 +6,45 @@ import ImageIcon from '@mui/icons-material/Image';
 import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
 import EventNoteIcon from '@mui/icons-material/EventNote';
 import CalendarViewDayIcon from '@mui/icons-material/CalendarViewDay';
+import Post from './Post';
+import { useState } from 'react';
 
 function Feed() {
-  return (
-    <div className='feed'>
-        <div className='feed__inputContainer'>
-            <div className='feed__input'>
-                <CreateIcon />
-                <form>
-                    <input type="text" />
-                    <button type='submit'>Send</button>
-                </form>
-            </div>
-            <div className='feed__inputOptions'>
-                <InputOption Icon={ImageIcon} title='Photo' color="#70B5F9"/>
-                <InputOption Icon={SubscriptionsIcon} title='Video' color="#E7A33E"/>
-                <InputOption Icon={EventNoteIcon} title='Event' color="#C0CBCD"/>
-                <InputOption Icon={CalendarViewDayIcon} title='Write article' color="#7FC15E"/>
-            </div>
-        </div>
+    const [posts, setPosts] = useState([])
 
-        {/* Posts */}
-    </div>
-  )
+    const sendPost = e => {
+        e.preventDefault();
+    }
+
+    return (
+        <div className='feed'>
+            <div className='feed__inputContainer'>
+                <div className='feed__input'>
+                    <CreateIcon />
+                    <form>
+                        <input type="text" />
+                        <button onClick={sendPost} type='submit'>Send</button>
+                    </form>
+                </div>
+                <div className='feed__inputOptions'>
+                    <InputOption Icon={ImageIcon} title='Photo' color="#70B5F9" />
+                    <InputOption Icon={SubscriptionsIcon} title='Video' color="#E7A33E" />
+                    <InputOption Icon={EventNoteIcon} title='Event' color="#C0CBCD" />
+                    <InputOption Icon={CalendarViewDayIcon} title='Write article' color="#7FC15E" />
+                </div>
+            </div>
+
+            {/* Posts */}
+            {posts.map((post) => (
+                <Post />
+            ))}
+            <Post
+                name="Marvin Merida"
+                description="This is a test"
+                message="Wow it works" 
+            />
+        </div>
+    )
 }
 
 export default Feed
